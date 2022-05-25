79ef01382749de27079d2ef1c3588451ac92043b
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-05-25 10:06:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stockfish_chess_engine
+
+Use Stockfish chess engine in your Flutter project.
+
+This project is based on sources for Stockfish 15.
---
+++ example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:stockfish_chess_engine/stockfish_chess_engine.dart' as stockfish_chess_engine;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late int sumResult;
+  late Future<int> sumAsyncResult;
+
+  @override
+  void initState() {
+    super.initState();
+    sumResult = stockfish_chess_engine.sum(1, 2);
+    sumAsyncResult = stockfish_chess_engine.sumAsync(3, 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 25);
+    const spacerSmall = SizedBox(height: 10);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Native Packages'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                const Text(
+                  'This calls a native function through FFI that is shipped as source in the package. '
+                  'The native code is built as part of the Flutter Runner build.',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+                Text(
+                  'sum(1, 2) = $sumResult',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+                FutureBuilder<int>(
+                  future: sumAsyncResult,
+                  builder: (BuildContext context, AsyncSnapshot<int> value) {
+                    final displayValue =
+                        (value.hasData) ? value.data : 'loading';
+                    return Text(
+                      'await sumAsync(3, 4) = $displayValue',
+                      style: textStyle,
+                      textAlign: TextAlign.center,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
